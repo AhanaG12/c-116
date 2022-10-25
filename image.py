eae2aa9c908b7853961381e936a631c64633f866
AhanaG12 <89616026+AhanaG12@users.noreply.github.com>
2022-10-24 18:20:11 -0700

    Add files via upload

---
+++ image.py
@@ -0,0 +1,4 @@
+import cv2
+img=cv2.imread("solar-system.jpg")
+cv2.imshow("solar-system.jpg",img)
+print(img)
---
+++ solar.py
@@ -0,0 +1,67 @@
+import numpy as np
+import cv2
+
+cv2.putText(img,
+"Sun",
+(20, 300),
+cv2.FONT_HERSHEY_COMPLEX,
+0.5,
+(255,255,255)
+)
+
+cv2.putText(img,
+"Mercury",
+(40, 300),
+cv2.FONT_HERSHEY_COMPLEX,
+0.5,
+(255,255,255)
+)
+
+cv2.putText(img,
+"Venus",
+(60, 300),
+cv2.FONT_HERSHEY_COMPLEX,
+0.5,
+(255,255,255)
+)
+
+cv2.putText(img,
+"Earth",
+(80, 300),
+cv2.FONT_HERSHEY_COMPLEX,
+0.5,
+(255,255,255)
+)
+
+cv2.putText(img,
+"Mars",
+(100, 300),
+cv2.FONT_HERSHEY_COMPLEX,
+0.5,
+(255,255,255)
+)
+
+cv2.putText(img,
+"Jupiter",
+(120, 300),
+cv2.FONT_HERSHEY_COMPLEX,
+0.5,
+(255,255,255)
+)
+
+cv2.putText(img,
+"Saturn",
+(140, 300),
+cv2.FONT_HERSHEY_COMPLEX,
+0.5,
+(255,255,255)
+)
+
+cv2.putText(img,
+"Neptune",
+(160, 300),
+cv2.FONT_HERSHEY_COMPLEX,
+0.5,
+(255,255,255)
+)
+
